82cf0045499943c52766959c1380ea7747a6165e
esrael23 <93195740+esrael23@users.noreply.github.com>
2023-08-25 03:19:52 -0700

    sidebar commit

---
+++ sidebar-MUI/README.md
@@ -0,0 +1,3 @@
+## sidebar using react js and material UI(MUI)
+
+The webpage features a navigation bar, within which resides a hamburger icon. Upon clicking this icon, the Material-UI (MUI) sidebar seamlessly emerges.
---
+++ sidebar-MUI/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ButtonAppBar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <ButtonAppBar/>
+    </div>
+  );
+}
+
+export default App;
